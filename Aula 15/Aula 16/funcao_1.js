25342bbb824f8eb819ba8e33fef74247993eba89
Abraão Borges <abraao10clear@gmail.com>
2023-08-15 16:08:18 -0300

    first commit

---
+++ Aula 15/Aula 16/funcao_1.js
@@ -0,0 +1,24 @@
+// Função ParImpar
+
+/* function parimpar (a) {
+    if (a%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+let res = parimpar (20)
+console.log (res)
+*/
+
+function imparpar (n) {
+    if(n%2 == 1){
+        return 'Impar'
+    } else {
+        return 'Par'
+    }
+}
+
+console.log (imparpar(90))
+
---
+++ Aula 15/composta.js
@@ -0,0 +1,11 @@
+// Aula 15 – Variáveis Compostas:
+
+let num = [5, 8, 2, 9, 3]
+
+
+num.sort() // coloca os números em ordem crescente
+num.push(1) // acrescenta um número
+console.log (num)
+console.log (`O vetor tem ${num.length} posiçoes`)
+console.log (`O primeiro valor do vetor é ${num[1]}`)
+
